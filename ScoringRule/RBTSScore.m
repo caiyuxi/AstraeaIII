bf963dba12eec3cceb7b1ff406d7611455f1025d
caiyuxi <yuxijune.cai@mail.utoronto.ca>
2019-08-19 16:15:51 -0400

    clean up and re-org RBTS rules

--- ScoringRule/RBTSScore.m
+++ ScoringRule/RBTSScore.m
@@ -1,38 +1,46 @@
 function u = RBTSScore(x,y)
-%RBTSSCORE based on the original proposal
+%RBTSSCORE based on the original RBTS proposal
 %   
-    for i = 1:length(x)
-        if i + 1 == length(x)
+    n = length(x);
+    assert(length(x) == length(y),'x and y must be of the same length');
+    
+    for i = 1:n
+        % find out j:  
+        % considering array index is 1-based in matlab,
+        % excluding corner case when j = n = i + 1
+        if i + 1 == n
             j = i + 1;
         else
-            j = mod(i+1,length(x));
+            j = mod(i+1,n);
         end
-        if i + 2 == length(x)
+        % find out k:  
+        % considering array index is 1-based in matlab,
+        % excluding corner case when k = n = i + 2
+        if i + 2 == n
             k = i + 2;
         else
-            k = mod(i+2,length(x));
+            k = mod(i+2,n);
         end
         
         yPrim = findYPrim(x(i), y(j));
-        u(i) = quadraticScoringRule(x(k), yPrim) + quadraticScoringRule(x(k), y(i));
+        u(i) = quadraticScoringRule(x(k), yPrim) ... % information score
+            + quadraticScoringRule(x(k), y(i));      % prediction score
         
     end
 end
 
+% helper function to findout yi' from the original proposal
 function yiPrim = findYPrim(xi,yj)
-    if yj > 0.5
-        delta = 1-yj;
-    else
-        delta = yj;
-    end
+    delta = min(1-yj, yj);
     
     if xi == 1
-        yiPrim = yj + delta;
+        yiPrim = yj + delta; % adjust up if information report is high
     else
-        yiPrim = yj - delta;
+        yiPrim = yj - delta; % adjust down if information report is low
     end
 end
 
+% classic quadratic scoring rule
 function score = quadraticScoringRule(xi,yi)
     if xi == 1
         score = 2 * yi - yi^2;
--- ScoringRule/RBTSScore_mean.m
+++ ScoringRule/RBTSScore_mean.m
@@ -1,41 +1,50 @@
 function u = RBTSScore_mean(x,y)
-%RBTSSCORE instead of reference and peer agent, use mean values (excluding the current agent)
+%RBTSSCORE instead of reference and peer agent, use means as the information
+%and prediction references. This still requires a minimum of 3 agents. 
 %   
-    for i = 1:length(x)
-        yRef= ((prod(y) / y(i))) ^ (1/(length(y) - 1));
-        xFreq = (sum(x) - x(i)) / (length(x) - 1);
+    prodY = prod(y);
+    sumX = sum(x);
+    n = length(x);
+    assert(length(x) == length(y),'x and y must be of the same length');
+    
+    for i = 1:n
+        % calculate the mean (excluding the current agent) for information 
+        % and prediction reports respectively
+        yRef  = (prodY / y(i)) ^ (1/(n - 1));
+        xFreq = (sumX - x(i)) / (n - 1);
+        % choose the popular side as x reference
         if xFreq < 0.5
             xRef = 0;
         elseif xFreq > 0.5
             xRef = 1;
         else
-            if i + 2 == length(x)
+            % if 50:50, choose reference agent based on the original proposal
+            if i + 2 == n
                 xRef = x(i + 2);
             else
-                xRef = x(mod(i+1,length(x)));
+                xRef = x(mod(i+2,n));
             end
         end
         
         yPrim = findYPrim(x(i), yRef);
-        u(i) = quadraticScoringRule(xRef, yPrim) + quadraticScoringRule(xRef, y(i));
+        u(i) = quadraticScoringRule(xRef, yPrim)... % information score
+            + quadraticScoringRule(xRef, y(i));     % prediction score
         
     end
 end
 
+% helper function to findout yi' from the original proposal
 function yiPrim = findYPrim(xi,yj)
-    if yj > 0.5
-        delta = 1-yj;
-    else
-        delta = yj;
-    end
+    delta = min(1-yj, yj);
     
     if xi == 1
-        yiPrim = yj + delta;
+        yiPrim = yj + delta; % adjust up if information report is high
     else
-        yiPrim = yj - delta;
+        yiPrim = yj - delta; % adjust down if information report is low
     end
 end
 
+% classic quadratic scoring rule
 function score = quadraticScoringRule(xi,yi)
     if xi == 1
         score = 2 * yi - yi^2;
